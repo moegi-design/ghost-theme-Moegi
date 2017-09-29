80aaba29d8d85fd2e061ab32d9515fcb90c4164f
ddiu8081 <ddiu8081@gmail.com>
2017-09-29 22:55:11 +0800

    Adjusted font weight

--- assets/js/moegi.js
+++ assets/js/moegi.js
@@ -1,10 +1,10 @@
-window.onscroll=function(){
+window.onscroll = function () {
     var goTop = document.getElementById("go-to-top");
-    if(goTop){
-        goTop.style.display = document.documentElement.scrollTop >= 200 || document.body.scrollTop >= 200 ? 'block':'none';
-        goTop.onclick=function(){
-            document.body.scrollTop=0;
-            document.documentElement.scrollTop=0;
-        }
+    if (goTop) {
+        goTop.style.display = document.documentElement.scrollTop >= 200 || document.body.scrollTop >= 200 ? 'block' : 'none';
+        goTop.onclick = function () {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        };
     }
-}
+};
