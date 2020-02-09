0ef2a1f06970791d228e0f137534b236cd39c886
ddiu8081 <ddiu8081@gmail.com>
2020-02-09 18:12:19 +0800

    :tada: using yarn v2

--- gulpfile.js
+++ gulpfile.js
@@ -1,82 +1,81 @@
-const { series, src, dest, watch } = require('gulp')
-const concat = require('gulp-concat')
-const uglify = require('gulp-uglify')
-const stylus = require('gulp-stylus')
-const px2rem = require('stylus-px2rem')
-const babel = require('gulp-babel')
+const {series, watch, src, dest, parallel} = require('gulp');
+const pump = require('pump');
+
+// gulp plugins and utils
 const zip = require('gulp-zip')
-const del = require('del')
-const gscan = require('gscan')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+const concat = require('gulp-concat')
+const clean = require('gulp-clean')
+const stylus = require('gulp-stylus')
 
 const PATH = {
-  CSS: './src/assets/css/*',
-  JS: './src/assets/js/*.js',
-  JSLIB: './src/assets/js/lib/*.js'
+    output: 'dist',
+    src: 'src/',
+    template: ['package.json', 'src/**/*', '!src/assets/**'],
+    css: 'src/assets/css/*',
+    js: ['src/assets/js/lib/*', 'src/assets/js/*']
 }
 
-function clean() {
-  del.sync(['./dist/*'])
-  return Promise.resolve()
+function cleanDir(done) {
+    pump([
+        src(PATH.output),
+        clean()
+    ], handleError(done))
 }
 
-function packTemplate() {
-  src('./package.json')
-    .pipe(dest('./dist/'))
-  src('./src/partials/*')
-    .pipe(dest('./dist/partials/'))
-  src('./src/*.hbs')
-    .pipe(dest('./dist/'))
-  src('./src/locales/*')
-    .pipe(dest('./dist/locales/'))
-  return Promise.resolve()
+function handleError(done) {
+    return function (err) {
+        if (err) {
+            console.error(err)
+        }
+        return done(err)
+    }
 }
 
-function packJs() {
-  src([PATH.JSLIB, PATH.JS])
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    .pipe(concat('moegi.js'))
-    .pipe(uglify())
-    .pipe(dest('./dist/assets/'))
-  return Promise.resolve()
+function template(done) {
+    pump([
+        src(PATH.template),
+        dest(PATH.output)
+    ], handleError(done))
 }
 
-function packCss() {
-  return src(PATH.CSS)
-    .pipe(stylus({
-      use: [px2rem()],
-      compress: true
-    }))
-    .pipe(concat('moegi.css'))
-    .pipe(dest('./dist/assets/'))
+function css(done) {
+    pump([
+        src(PATH.css, {sourcemaps: true}),
+        stylus(),
+        dest(`${PATH.output}/assets`, {sourcemaps: '.'})
+    ], handleError(done))
 }
 
-function generateZip() {
-  return src('./dist/*')
-    .pipe(zip('./moegi.zip'))
-    .pipe(dest('./'))
+function js(done) {
+    pump([
+        src(PATH.js, {sourcemaps: true}),
+        babel({
+            presets: ['@babel/env']
+        }),
+        concat('screen.js'),
+        uglify(),
+        dest(`${PATH.output}/assets`, {sourcemaps: '.'})
+    ], handleError(done))
 }
 
-function scan() {
-  gscan.checkZip({
-    path: './moegi.zip',
-    // if you need to check the theme for a different
-    // major Ghost version, you can pass it. Currently
-    // v1, which is Ghost 1.0 is supported. Default is
-    // the latest Ghost version 2.0:
-    // checkVersion: 'v1',
-    name: 'my-theme'
-  }).then(function (result) {
-    console.log(result)
-  }).catch(function(err) {
-    console.log(err)
-  })
+function zipper(done) {
+    var targetDir = './'
+    var themeName = require('./package.json').name
+    var filename = themeName + '.zip'
+
+    pump([
+        src(PATH.output),
+        zip(filename),
+        dest(targetDir)
+    ], handleError(done))
 }
 
-function watchFiles() {
-  watch('./src/**/*', series(clean, packTemplate, packJs, packCss))
-}
+const build = series(cleanDir, template, css, js)
+const watcher = () => watch(PATH.src, build)
+const dev = series(build, watcher)
 
-exports.default = series(clean, packTemplate, packJs, packCss, generateZip)
-exports.watch = watchFiles;
+exports.build = build;
+exports.zip = series(build, zipper)
+exports.default = dev
