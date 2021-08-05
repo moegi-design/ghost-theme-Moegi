bf153b14b50f5514b711327f2cece566e8af94d9
ddiu8081 <ddiu8081@gmail.com>
2021-08-05 18:05:20 +0800

    feat(ui): index & post page basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{hbs,js}', './template/**/*.{hbs,js}'],
   darkMode: 'class',
   theme: {
+    colors: {
+      accent: 'var(--ghost-accent-color)',
+      ...colors,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
