6a28d4edd656976f5434c5c42102bbee03f77689
ddiu8081 <ddiu8081@gmail.com>
2022-01-13 21:27:49 +0800

    build: upgrade TailwindCSS to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,16 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./src/**/*.{hbs,js}', './template/**/*.{hbs,js}'],
+  content: [
+    './src/**/*.{hbs,js}', './template/**/*.{hbs,js}'
+  ],
   darkMode: 'class',
   theme: {
-    colors: {
-      accent: 'var(--ghost-accent-color)',
-      ...colors,
+    extend: {
+      colors: {
+        main: 'var(--ghost-accent-color)',
+      },
     },
-    extend: {},
-  },
-  variants: {
-    extend: {},
   },
   plugins: [
     require('@tailwindcss/typography'),
